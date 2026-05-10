2666021938186ff0c6daa36aecbfc3cd6a5450a0
Loyiso Phura <phuthumaloyisopetse@gmail.com>
2026-05-10 13:35:59 +0000

    created a people file for family

---
+++ models/people/people.sql
@@ -0,0 +1,5 @@
+select 'Phuthuma' as name, 'Petse' as surname, '11-08-1988'
+union all
+select 'Iminathi' as name, 'Petse' as surname, '10-08-2011'
+union all
+select 'Lunako' as name, 'Petse' as surname, '18-05-2015'
